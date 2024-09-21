a9c37c31b9d665e57dcb702f41729fbbe4f35330
mtb9515 <mtb9515@mavs.uta.edu>
2024-09-21 16:52:57 -0500

    removed a comment

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import './Map.css';  // Assuming you have basic CSS for layout
+import './Map.css'; 
 
 const Map = () => {
   // Set the initial map view (latitude, longitude, zoom level)
