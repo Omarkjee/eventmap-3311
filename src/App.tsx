027caaa0e74495e28a2e2457cd361079ee3f9328
Mason <mtb9515@mavs.uta.edu>
2024-11-12 11:46:23 -0600

    removed lines that shouldnt have been re-added

--- src/App.tsx
+++ src/App.tsx
@@ -77,7 +77,8 @@ function App() {
     if (pathParts[1] === 'events' && eventIdFromUrl) {
       setActiveSection('viewEvent');
       setSelectedEventId(eventIdFromUrl);
-
+      localStorage.setItem('activeSection', 'viewEvent');
+      localStorage.setItem('selectedEventId', eventIdFromUrl);
     }
   }, [location.pathname]);
 
